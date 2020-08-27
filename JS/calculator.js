d25e62c7b907dc6f05c2318f21175d2ba6a40c72
Galxtime <gllrmacosta@yahoo.com>
2020-08-27 09:29:47 -0500

    add files calculator

---
+++ JS/calculator.js
@@ -0,0 +1,39 @@
+let value1
+let value2
+function add(){
+        value1 =document.getElementById("n1").value;
+        value2=document.getElementById("n2").value;
+      
+        resultado=parseFloat(value1)+parseFloat(value2);
+        alert(resultado);
+    }
+
+function sub(){
+        value1 =document.getElementById("n1").value;
+        value2=document.getElementById("n2").value;
+      
+        resultado=parseFloat(value1)-parseFloat(value2);
+        alert(resultado);
+    
+  
+}
+
+function mux(){
+        value1 =document.getElementById("n1").value;
+        value2=document.getElementById("n2").value;
+      
+        resultado=parseFloat(value1)*parseFloat(value2);
+        alert(resultado);
+    
+  
+}
+
+function div(){
+        value1 =document.getElementById("n1").value;
+        value2=document.getElementById("n2").value;
+      
+        resultado=parseFloat(value1)/parseFloat(value2);
+        alert(resultado);
+    
+  
+}
